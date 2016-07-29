7aa3ea9f0ebd8180fcbd9d44a0650bbf41c5538c
RitaDp <rmsdnepr@gmail.com>
2016-07-29 15:07:15 +0300

    w

--- module4/src/main/java/com/courses/spalah/PrintMatrix.java
+++ module4/src/main/java/com/courses/spalah/PrintMatrix.java
@@ -51,35 +51,51 @@ public class PrintMatrix {
     public static void main(String[] args) {
 
         int size = 5;
-        char b = '#';
-        char[][] a = new char[size][size];
+
+        for(int i = 0; i < size; i++) {
+            for(int k = 0; k < size; k++) {
+                System.out.print("#" + " ");
+            }
+            System.out.println();
+        }
+
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                a[i][j] = b;
-                System.out.print(a[i][j] + " ");
+            for (int b = 0; b < i + 1; b++) {
+                System.out.print("#" + " ");
             }
             System.out.println();
         }
 
-        char b1 = '#';
-        int[][] a1 = new int[5][5];
-        /*a1[0] = new int [1];
-        a1[1] = new int [2];
-        a1[2] = new int [3];
-        a1[3] = new int [4];
-        a1[4] = new int [5];
-        */
-
-        for(int i = 0; i<a1.length; i++){
-            for(int j = 0; j<a1[i].length; j++){
-                a1[i][j] = b1;
-                System.out.print(a1[i][j] + " ");
+        for (int i = size; i >= 0; i--) {
+            for (int b = 0; b < i + 1; b++) {
+                System.out.print("#" + " ");
             }
             System.out.println();
         }
 
-    }
+        for (int i = 0; i < size; i++) {
+            for (int j = size; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+        for(int i = 0; i <= size; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = size; k >= i; k--)
+            {
+                System.out.print("#");
+            }
+            System.out.println();
+   }
 }
+}
+
 
 
 
